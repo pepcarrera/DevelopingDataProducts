a358dd45f502ca61567454a791998d982042fda9
Pep Carrera <pepccarrera@gmail.com>
2014-10-10 09:23:47 -0500

    through shiny lecture

--- Example2/server.R
+++ Example2/server.R
@@ -1,7 +1,7 @@
+diabetesRisk <- function(glucose) glucose / 200
 shinyServer(
         function(input, output) {
-                output$oid1 <- renderPrint({input$id1})
-                output$oid2 <- renderPrint({input$id2})
-                output$odate <- renderPrint({input$date})
+                output$inputValue <- renderPrint({input$glucose})
+                output$prediction <- renderPrint({diabetesRisk(input$glucose)})
         }
 )
--- Example2/ui.R
+++ Example2/ui.R
@@ -1,20 +1,17 @@
-shinyUI(pageWithSidebar(
-        headerPanel("Illustrating inputs"),
-        sidebarPanel(
-                numericInput('id1', 'Numeric input, labeled id1', 0, min = 0, max = 10, step = 1),
-                checkboxGroupInput("id2", "Checkbox",
-                                   c("Value 1" = "1",
-                                     "Value 2" = "2",
-                                     "Value 3" = "3")),
-                dateInput("date", "Date:")
-        ),
-        mainPanel(
-                h3('Illustrating outputs'),
-                h4('You entered'),
-                verbatimTextOutput("oid1"),
-                h4('You entered'),
-                verbatimTextOutput("oid2"),
-                h4('You entered'),
-                verbatimTextOutput("odate")
+shinyUI(
+        pageWithSidebar(
+                # Application title
+                headerPanel("Diabetes prediction"),
+                sidebarPanel(
+                        numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
+                        submitButton('Submit')
+                ),
+                mainPanel(
+                        h3('Results of prediction'),
+                        h4('You entered'),
+                        verbatimTextOutput("inputValue"),
+                        h4('Which resulted in a prediction of '),
+                        verbatimTextOutput("prediction")
+                )
         )
-))
+)
