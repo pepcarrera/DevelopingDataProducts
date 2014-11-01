b50482416b01fb47c229b42c0d85b06b770313b3
Pep Carrera <pepcarrera@gmail.com>
2014-11-01 16:41:13 -0500

    quiz4

---
+++ quiz1.R
@@ -0,0 +1,13 @@
+library(manipulate)
+myPlot <- function(s) {
+        plot(cars$dist - mean(cars$dist), cars$speed - mean(cars$speed))
+        abline(0, s)
+}
+manipulate(myPlot(s), s = slider(0, 2, step = 0.1))
+
+
+
+
+library(rCharts)
+data(airquality)
+dTable(airquality, sPaginationType = "full_numbers")
